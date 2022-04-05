61ce5589683c9d38378875a199db73b8f17158aa
anfalfadhil <87868348+anfalfadhil@users.noreply.github.com>
2022-04-05 09:55:22 -0400

    py

--- makeup_backend/settings.py
+++ makeup_backend/settings.py
@@ -157,7 +157,7 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
-DEBUG = False
+# DEBUG = False
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
